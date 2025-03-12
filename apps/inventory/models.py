4d93e109f9517a86bc423dbb35e568441af58246
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-03-12 13:14:21 +0200

    Add inventory_adjustment model

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from apps.products.models import Product
+class InventoryAdjustment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    reason = models.CharField(max_length=255)
+    adjustment_type = models.CharField(max_length=30)
+    value = models.DecimalField(max_digits=18, decimal_places=2)
