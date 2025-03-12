5418ffaef072ed497088ba3659a8d262d6a83839
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-03-12 13:16:33 +0200

    Add product model

--- apps/purchase/models.py
+++ apps/purchase/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from apps.products.models import Product
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    cost = models.DecimalField(max_digits=18, decimal_places=2)
+    date = models.DateField()
