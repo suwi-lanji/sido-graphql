f57bb0059a3e046451652f9b2d81b5b0ffd3067b
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-03-12 13:03:26 +0200

    Add sale model

--- apps/sales/models.py
+++ apps/sales/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from apps.products.models import Product
+class Sale(models.Model):
+    quantity = models.IntegerField()
+    total_amount = models.DecimalField(max_digits=18, decimal_places=2)
+    sale_date = models.DateField()
+    products = models.ManyToManyField(Product)
 
-# Create your models here.
+    def __str__(self):
+        return f"sale_on_{self.sale_date}"
