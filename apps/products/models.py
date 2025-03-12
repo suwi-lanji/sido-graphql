bc2b601c4939068a628892131a04daab3611da96
Suwilanji Chipofya <suwilanjichipofya@outlook.com>
2025-03-12 12:19:38 +0200

    Add product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=18, decimal_places=2)
+    safety_quauntity = models.IntegerField(default=1)
+    quantity_in_stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"product_{self.name}_k{self.price}"
